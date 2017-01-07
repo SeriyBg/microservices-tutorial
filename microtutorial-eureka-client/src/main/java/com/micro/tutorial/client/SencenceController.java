4b6307a43d4f6f55a7b3ea466dc0655f1a4d6307
Serge Bishyr <seriybg@gmail.com>
2017-01-07 11:29:50 +0200

    Add ribbon load balancer

--- microtutorial-eureka-client/src/main/java/com/micro/tutorial/client/SencenceController.java
+++ microtutorial-eureka-client/src/main/java/com/micro/tutorial/client/SencenceController.java
@@ -3,19 +3,22 @@ package com.micro.tutorial.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 public class SencenceController {
 
     @Autowired
     private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
 
     @RequestMapping("/sentence")
     public @ResponseBody
@@ -30,12 +33,10 @@ public class SencenceController {
     }
 
     public String getWord(String service) {
-        List<ServiceInstance> list = discoveryClient.getInstances(service);
-        if (list != null && list.size() > 0 ) {
-            URI uri = list.get(0).getUri();
-            if (uri !=null ) {
-                return (new RestTemplate()).getForObject(uri,String.class);
-            }
+        ServiceInstance serviceInstance = loadBalancerClient.choose(service);
+        URI uri = serviceInstance.getUri();
+        if (uri !=null ) {
+            return (new RestTemplate()).getForObject(uri,String.class);
         }
         return null;
     }
